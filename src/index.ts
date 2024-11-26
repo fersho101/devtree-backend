1551f2ad550fbd5c0ece3b9ae612708dc3675cee
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-26 06:55:05 -0700

    :construction: routing

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ const port = process.env.PORT || 4000
 
 
 server.listen(port, () => {
-    // console.log('Servidor funcionando en el puerto ' + port)
     console.log(`Servidor funcionando en puerto ${port}`)
 })
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express'
 
 const router = Router()
+
+// Autenticacion y registro
+
+router.post('/auth/register', (req, res) => {
+     console.log("Desde registro")
+})
 
 
 
