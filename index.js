1fa20da228ddbb6892891ff4055c8689edc86e99
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-25 23:05:25 -0700

    intro express, install nodemon

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo desde devtree')
+// const express = require('express') // CJS Common Js
+import express from 'express' // ESM EcmaScript modules
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo en express')
+})
+
+app.listen(port, () => {
+    // console.log('Servidor funcionando en el puerto ' + port)
+    console.log(`Servidor funcionando en puerto ${port}`)
+})
