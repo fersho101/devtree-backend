66f01903f70fa43dd8d914c7db756a158a9e7362
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-26 12:40:44 -0700

    :construction: model router

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express'
+import User from './models/User'
 
 const router = Router()
 
 // Autenticacion y registro
 
-router.post('/auth/register', (req, res) => {
-     console.log("Desde registro")
+router.post('/auth/register', async (req, res) => {    
+     // await User.create(req.body)
+     const user = new User(req.body)
+     await user.save()
+     res.send('Registro creado correctamente')
 })
-
-
 
 export default router
