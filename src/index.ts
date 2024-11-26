a3773d22089a814ac5b40ec462f1923111070228
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-25 23:38:40 -0700

    configuraciones ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-// const express = require('express') // CJS Common Js
+
 import express from 'express' // ESM EcmaScript modules
 
 const app = express()
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo en express')
+    res.send('Hola mundo en express / typescript')
 })
 
 app.listen(port, () => {
