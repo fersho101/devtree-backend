f9605edb994a1382673b7ee68ba8c4e7d68763d5
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-11-26 10:27:32 -0700

    :art:

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
+import colors from 'colors'
 import server from './server'
 
 const port = process.env.PORT || 4000
 
-
-
 server.listen(port, () => {
-    console.log(`Servidor funcionando en puerto ${port}`)
+    console.log(colors.magenta.italic.bold(`Servidor funcionando en puerto: ${port}`))
 })
