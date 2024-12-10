11e1d3b966f0ac56f3451530f56ab2428bbc4bd6
fernando corral <146652466+fersho101@users.noreply.github.com>
2024-12-09 18:08:38 -0700

    :construction:

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express'
-import User from './models/User'
+import {body} from 'express-validator'
+import { createAccount } from './handlers'
 
 const router = Router()
 
 // Autenticacion y registro
+router.post('/auth/register',
+    body('handle')
+        .notEmpty()
+        .withMessage('El handle no puede ir vacio'),
+    body('name')
+        .notEmpty()
+        .withMessage('El Nombre no puede ir vacio'),
+    body('email')
+        .isEmail()
+        .withMessage('email no valido'),
+    body('password')
+        .isLength({min:8})
+        .withMessage('El password no puede ir vacio, minimo 8 caracteres'),
+    createAccount )
 
-router.post('/auth/register', async (req, res) => {    
-     // await User.create(req.body)
-     const user = new User(req.body)
-     await user.save()
-     res.send('Registro creado correctamente')
-})
-
-export default router
+export default router 
